eb7a0eda33f265b423b2b21aa88e31fe1a6f3188
YeuChesen <yeuchesen@gmail.com>
2019-11-08 14:46:31 +0700

    Use reduce to filter array

--- w1/makedata.js
+++ w1/makedata.js
@@ -20,6 +20,15 @@ let nameArr = args.map((arg, i) => {
     return null
 }).filter(it => !!it)
 
+
+// YeuChesen: check validation for (index + 1) > array length and after "-n" is "-***" 
+//example input array: ["node", "srcPath", "-n", "abc", "-2", "12", "-n", "456", "-b", "7", "-n", "-a"]
+//output: ["abc", "456"]
+let tempArr = test.reduce((rs, itm, idx) => {
+    if (itm === '-n' && ((idx + 1)< test.length) && (!test[idx+1].toString().includes('-'))) rs.push(test[idx+1]);
+    return rs;
+  }, []);
+
 // => node srcPath -n abc -2 12 -n 456 -b 7
 // map: nul nul abc nul nul 456 nul nul
 // filter: [ 'abc', '456' ]
