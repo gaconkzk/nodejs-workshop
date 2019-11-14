bcf4eab3f7253431426f720fbf6ccde71a381a75
TMA\ntrucanh <ntrucanh@tma.com.vn>
2019-11-14 14:06:18 +0700

    home work w1

--- w1/makedata.js
+++ w1/makedata.js
@@ -12,17 +12,29 @@ let args = process.argv
 //         return args[i + 1]
 //     })
 
-let nameArr = args.map((arg, i) => {
-    if (arg === '-n') {
-        return args[i + 1]
-    }
+// let nameArr = args.map((arg, i) => {
+//     if (arg === '-n') {
+//         return args[i + 1]
+//     }
 
-    return null
-}).filter(it => !!it)
+//     return null
+// }).filter(it => !!it)
 
 // => node srcPath -n abc -2 12 -n 456 -b 7
 // map: nul nul abc nul nul 456 nul nul
 // filter: [ 'abc', '456' ]
 // reduce:
 
+// console.log("-n array: ", nameArr)
+// let temp = [];
+
+//  let nameArr = args.filter((arg, i) => {
+//     if (arg === '-n' && args[i+1]) {
+//         temp.push(args[i + 1])
+//     }
+//     return arg;
+//  })
+//  console.log("-n array: ", temp)
+
+let nameArr = args.filter((arg, i) => args[i - 1] === '-n')
 console.log("-n array: ", nameArr)
