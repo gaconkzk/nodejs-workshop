6602f4030bdb8b6777d3887bda94b0cbd0f059a6
Vincent To <gaconkzk@gmail.com>
2019-11-15 08:11:14 +0700

    Update async_cli_parser.js

--- w2/async/async_cli_parser.js
+++ w2/async/async_cli_parser.js
@@ -1,8 +1,8 @@
-let isValidKey = (idx, arr) => (idx) < arr.length && !arr[idx].startsWith('-')
+let isValue = (item) => !item.startsWith('-')
 
 function getValueByKey(key, arrs) {
   return arrs
-    .find((_, i) => arrs[i - 1] === `-${key}` && isValidKey(i, arrs))
+    .find((item, i) => arrs[i - 1] === `-${key}` && isValue(item))
 }
 
 module.exports = {
