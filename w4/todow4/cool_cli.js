efd4cf33f2ad57b23e7c30eab5ac7d65f987d9da
YeuChesen <yeuchesen@gmail.com>
2019-12-10 17:20:29 +0700

    Updated: server load img/txt/pdf ....

--- w4/todow4/cool_cli.js
+++ w4/todow4/cool_cli.js
@@ -1,45 +1,19 @@
 require('dotenv').config({path:__dirname+'/./../../.env'})
 const server = require('./http')
-const utils = require('./res-utils')
-const argv = require('yargs') // eslint-disable-line
-    .option('port', {
-        alias: 'p',
-        default: process.env.PORT,
-        type: 'number',
-        description: 'Server port (default: 8080)'
-    })
-    .option('ip', {
-        alias: 'i',
-        default: process.env.IP,
-        type: 'string',
-        description: 'Server external ip (default: 127.0.0.1)'
-    })
-    .option('folder', {
-        alias: '-f',
-        default: process.env.FOLDER,
-        type: 'string',
-        description: '(The default folder that server will serve static files (default: public)'
-    }).help('help')
-   .argv
-   let port = argv.port;
-   let ip = argv.ip;
-   let folder = argv.folder;
-    if (!utils.checkPort(port)) {
-        port = process.env.PORT
-        console.log(`Port ${argv.port} is invalid, server will start with ${port}`)
-    }
-    if (!utils.checkIp(ip)) {
-        ip = process.env.IP
-        console.log(`IP ${argv.ip} is invalid, server will start with ${ip}`)
-    }
-    if (!utils.checkFolderExist(folder)) {
-        folder = process.env.FOLDER
-        console.log(`Folder ${argv.folder} is not exist, server will start with ${folder}`)
-    }
+const argvItems = require('./argv-items')
+const commonUtils = require('./common-utils')
 
-    server.startServer(port, ip, folder);
+const argv = require('yargs') // eslint-disable-line
+    .option(argvItems.port.key, argvItems.port)
+    .option(argvItems.ip.key, argvItems.ip)
+    .option(argvItems.folder.key, argvItems.folder).help('help')
+    .argv
+   console.log(argv.port, argv.ip, argv.folder)
+    let svrInfo = commonUtils.serverInfo(argv.port, argv.ip, argv.folder); 
+
+    server.startServer(svrInfo.port, svrInfo.ip, svrInfo.folder);
    
-    console.log(`Server started: \n  - On port: ${port} \n  - External IP ${ip} \n  - Folder: ${folder}`);
+    console.log(`Server started: \n  - On port: ${svrInfo.port} \n  - External IP ${svrInfo.ip} \n  - Folder: ${svrInfo.folder}`);
 
 // TODO create cli to start an http server
 // cli params:
